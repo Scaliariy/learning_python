621cc68a087f54ec56c0c5d631299370a1055e82
Nicol <scaliariy1@gmail.com>
2023-09-10 20:32:38 +0300

    part 13 commit

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-def get_todos():
-    # context provider/manager:
-    with open('files/todos.txt', 'r') as file_local:
+def get_todos(filepath_local):
+    with open(filepath_local, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
+
+def write_todos(filepath_local, todos_local):
+    with open(filepath_local, 'w') as file_local:
+        file_local.writelines(todos_local)
+
+
+filepath = 'files/todos.txt'
 
 while True:
     user_action = input("Type show, add, edit, complete or exit: ")
 
     if user_action.startswith('add'):
         todo = user_action[4:]
-        todos = get_todos()
+        todos = get_todos(filepath)
         todos.append(todo + '\n')
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos(filepath, todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos()
-        # list comprehension:
-        # todos = [todo.strip('\n') for todo in todos]
+        todos = get_todos(filepath)
         for index, item in enumerate(todos):
             item = item.strip('\n')
-            # f-string:
             row = f"{index + 1}-{item}"
             print(row)
 
     elif user_action.startswith('edit'):
         try:
-            todos = get_todos()
+            todos = get_todos(filepath)
             number = int(user_action[5:])
             new_todo = input("New value is: ")
             todos[number - 1] = new_todo + '\n'
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(filepath, todos)
         except ValueError:
             print("Your command is not valid.")
             continue
 
     elif user_action.startswith('complete'):
         try:
-            todos = get_todos()
+            todos = get_todos(filepath)
             number = int(user_action[9:])
             removed_todo = todos.pop(number - 1).strip('\n')
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos(filepath, todos)
             message = f"Todo \"{removed_todo}\" was removed from the list."
             print(message)
         except IndexError:
