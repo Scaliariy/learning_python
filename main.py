91c7c7da9962828495e47d214b54e3145f5f18ed
Nicol <scaliariy1@gmail.com>
2023-09-11 15:11:28 +0300

    part 14 commit

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-def get_todos(filepath_local):
+def get_todos(filepath_local="files/todos.txt"):
     with open(filepath_local, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath_local, todos_local):
+def write_todos(todos_local, filepath_local="files/todos.txt"):
     with open(filepath_local, 'w') as file_local:
         file_local.writelines(todos_local)
 
-
-filepath = 'files/todos.txt'
 
 while True:
     user_action = input("Type show, add, edit, complete or exit: ")
 
     if user_action.startswith('add'):
         todo = user_action[4:]
-        todos = get_todos(filepath)
+        todos = get_todos()
         todos.append(todo + '\n')
-        write_todos(filepath, todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos(filepath)
+        todos = get_todos()
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}-{item}"
@@ -29,21 +27,21 @@ while True:
 
     elif user_action.startswith('edit'):
         try:
-            todos = get_todos(filepath)
+            todos = get_todos()
             number = int(user_action[5:])
             new_todo = input("New value is: ")
             todos[number - 1] = new_todo + '\n'
-            write_todos(filepath, todos)
+            write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
 
     elif user_action.startswith('complete'):
         try:
-            todos = get_todos(filepath)
+            todos = get_todos()
             number = int(user_action[9:])
             removed_todo = todos.pop(number - 1).strip('\n')
-            write_todos(filepath, todos)
+            write_todos(todos)
             message = f"Todo \"{removed_todo}\" was removed from the list."
             print(message)
         except IndexError:
