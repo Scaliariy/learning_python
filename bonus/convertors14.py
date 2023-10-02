890c4b8b44fe7d9c900e6b15d274adaf59664c66
Nicol <scaliariy1@gmail.com>
2023-10-02 20:02:01 +0300

    part 18.2

--- bonus/convertors14.py
+++ bonus/convertors14.py
@@ -1,3 +1,7 @@
 def convert(feet, inches):
-    meters = feet * 0.3048 + inches * 0.0254
+    if feet == '':
+        feet = 0
+    elif inches == '':
+        inches = 0
+    meters = float(feet) * 0.3048 + float(inches) * 0.0254
     return meters
--- bonus/gui_convertor.py
+++ bonus/gui_convertor.py
@@ -1,17 +1,28 @@
 import PySimpleGUI as sg
+from convertors14 import convert
 
 label1 = sg.Text("Enter feet:")
-input_box1 = sg.InputText()
+input_box1 = sg.InputText(key="feet")
 
 label2 = sg.Text("Enter inches:")
-input_box2 = sg.InputText()
+input_box2 = sg.InputText(key="inches")
 
 button = sg.Button("Convert")
+output = sg.Text(key="output", text_color="yellow")
 
 window = sg.Window("Convertor",
                    layout=[[label1, input_box1],
                            [label2, input_box2],
-                           [button]])
+                           [button, output]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    value = convert(values["feet"], values["inches"])
+    value = str(value) + ' m'
+    window["output"].update(value=value)
+
+    if event == sg.WINDOW_CLOSED:
+        break
 window.close()
+
